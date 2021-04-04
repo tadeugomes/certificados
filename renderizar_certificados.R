26893ca7ee8376298da6735e96fb1e371c4f729c
Tadeu Gomes <32282635+tadeugomes@users.noreply.github.com>
2021-04-04 15:58:44 -0300

    Add files via upload

--- renderizar_certificados.R
+++ renderizar_certificados.R
@@ -7,10 +7,10 @@
 
 library(purrr)
 
-render_nomes<-function(V1){
-  template <- "parametros.Rmd"  # template previamente criado rm Rmarkdown
-  outfile <- sprintf("Cert_Pibic_%s.pdf", V1) # usar a funcao para nomear os arquivos
-  parametros <- list (V1 = V1) # Lista com os parametros a serem usados para impressao
+render_nomes<-function(nome_completo, polo, papel){
+  template <- "parametros.Rmd"  # template previamente criado em Rmarkdown
+  outfile <- sprintf("Cert_Pibic_%s.pdf", nome_completo) # usar a funcao para nomear os arquivos
+  parametros <- list (nome_completo = nome_completo, polo = polo, papel = papel) # Lista com os parametros a serem usados para impressao
   rmarkdown::render(template, 
                     output_file = outfile,
                     params = parametros)
@@ -18,11 +18,11 @@ render_nomes<-function(V1){
 }
   
 # Teste com um nome nome aleatório presente na coluna V1
-render_nomes("Nome Completo")
+render_nomes()
 
-# Criar uma lista com os dados da coluna V1
-nomes <-list (arquivo.parametros$V1)
+  # Criar uma lista com os dados da coluna V1
+nomes <-list (arquivo.parametros$nome_completo, arquivo.parametros$polo, arquivo.parametros$papel)
 
 # A bruxaria acontece: serão gerados os certificados com os nomes presentes na coluna V1
 purrr::pmap(nomes, render_nomes)
-    
+                            
